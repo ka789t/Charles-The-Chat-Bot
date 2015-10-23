2b4a94e122b34bef24c5a4865066523005f1730d
melaniethomas9 <programmer@hs-lab-14.local>
2015-10-23 09:06:13 -0400

    more fine tuning and editing

--- MelKatChatbot.py
+++ MelKatChatbot.py
@@ -3,5 +3,19 @@ def start_conversation():
     name=raw_input("What's your name? ")
     print(name+", it is a pleasure to meet you.")
     feeling=raw_input("How are you feeling? "):
-    		if feeling in range("sad","mad","bad"):
-		   print("Sorry to hear that.")
+    		while feeling in range["sad","mad","bad"]:
+		   print("I am sorry to hear that.")
+                else:
+                    print("Nice.")
+
+def give_question():
+ #lst = ['What is your favorite color?','What is your favorite animal?','What is your favorite subject in school?','What is your favorite type of music?','What is your favorite movie?','Least favorite movie?']
+    lst = [['What is your favorite animal?','That is a great one.'],
+        ['What is your favorite color?','That is my favorite too!'],
+        ['What is your favorite subject in school?','I do not like that one.'],
+        ['What is your favorite type of music?','Mine too!'],
+        ['If you could be any mythical creature, what would you be?','I would want to be a dragon!'],
+        ['What is your favorite movie?','I wish I could watch movies...']]
+
+def good_bye():
+    print("It was nice talking, but I am going to go now. Bye!")
