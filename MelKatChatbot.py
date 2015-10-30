c16c3aa0a07e2237f5970846c9e730176af7431b
ka789t <programmer@hs-lab-12.local>
2015-10-30 09:11:43 -0400

    pretty print!!woop!

--- MelKatChatbot.py
+++ MelKatChatbot.py
@@ -1,7 +1,23 @@
-def tell_joke
-lst=['How do you drown a hipster? Throw him into the mainstream.'
-     ['Why did the computer get cold?','Because he forgot to close windows!!!'],
-     ['Why did the plane crash?','Because the pilot was a loaf of bread.'],
-     ['Why was the boy sad?','Because he had a frog stapled to his face.']]
+def pretty_print(txt):
+    print '*' * (len(txt)+4)
+    print '* '+text+' *'
+    print '*' * (len(txt)+4)
+    
+
+def tell_joke ():
+    lst=['How do you drown a hipster? Throw him into the mainstream.'
+         ['Why did the computer get cold?','Because he forgot to close windows!!!'],
+         ['Why did the plane crash?','Because the pilot was a loaf of bread.'],
+         ['Why was the boy sad?','Because he had a frog stapled to his face.']]
     selection= random.coice(lst)
     print selection
+
+def test_tell_function():
+    for n in range(10):
+        tell_joke()
+
+pretty_print('Welcome to Chatbot!')
+
+    
+
+    
