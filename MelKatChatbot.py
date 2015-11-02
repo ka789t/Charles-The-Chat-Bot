97b9521bdc64d369327d13a8ec16a2afec937b7a
ka789t <programmer@hs-lab-12.local>
2015-11-02 12:13:29 -0500

    beginning of test function for jokes and small edits of the joke and pretty print functions

--- MelKatChatbot.py
+++ MelKatChatbot.py
@@ -1,23 +1,24 @@
+import random
+
 def pretty_print(txt):
     print '*' * (len(txt)+4)
-    print '* '+text+' *'
+    print '* '+txt+' *'
     print '*' * (len(txt)+4)
     
 
 def tell_joke ():
-    lst=['How do you drown a hipster? Throw him into the mainstream.'
+    lst=[['How do you drown a hipster? Throw him into the mainstream.'
          ['Why did the computer get cold?','Because he forgot to close windows!!!'],
          ['Why did the plane crash?','Because the pilot was a loaf of bread.'],
          ['Why was the boy sad?','Because he had a frog stapled to his face.']]
-    selection= random.coice(lst)
+    selection = random.coice(lst)
     print selection
 
-def test_tell_function():
+def test_tell_joke():
     for n in range(10):
         tell_joke()
 
 pretty_print('Welcome to Chatbot!')
-
-    
+test_tell_joke()  
 
     
