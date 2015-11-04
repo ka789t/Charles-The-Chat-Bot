99e6670601c2bffbd4605462f95fff40de36ebbf
melaniethomas9 <programmer@hs-lab-14.local>
2015-11-04 09:57:37 -0500

    give_question
    editing on give_question function to make it delete items already stated

--- MelKatChatbot.py
+++ MelKatChatbot.py
@@ -11,23 +11,30 @@ def start_conversation():
         print("Nice.")
 
         
-def give_question():
+def give_question(already_asked):
+    '''give one question at a time. Don't ask a question in already_asked
+    '''
     lst = [['What is your favorite animal?','That is a great one.'],
         ['What is your favorite color?','That is my favorite too!'],
         ['What is your favorite subject in school?','I do not like that one.'],
         ['What is your favorite type of music?','Mine too!'],
         ['If you could be any mythical creature, what would you be?','I would want to be a dragon!'],
         ['What is your favorite movie?','I wish I could watch movies...']]
+    for n in already_asked:
+        lst.remove(n)
     selection = random.choice(lst)
     print selection
-    # IDK how to make the program use each question only once per game
-
+    return selection
+    
 def good_bye():
     print("It was nice talking, but I am going to go now. Bye!") 
 
 def test_give_question():
-    for n in range(18):
-       give_question()
+    already_asked = []
+    for n in range(6):
+        already_asked.append(give_question(already_asked))
 
-test_give_question()
-    
+def play_game():
+    start_conversation()
+    give_question(already_asked)
+    good_bye()
